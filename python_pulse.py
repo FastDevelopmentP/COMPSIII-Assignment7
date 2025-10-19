dd3a03816379b01221b9b19ec4137ea8edeebdc1
FastDevelopmentP <pete5fast@gmail.com>
2025-10-18 20:36:57 -0400

    Created python_pulse.db

--- python_pulse.py
+++ python_pulse.py
@@ -1,21 +1,14 @@
-# Write your code here
+import sqlite3
 
+connection = sqlite3.connect('python_pulse.db')
+cursor = connection.cursor()
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+''')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# DON'T DELETE THIS LINE - Commit the changes and close the connection. This should be the LAST line of your program.
-connection.commit()
-connection.close()
